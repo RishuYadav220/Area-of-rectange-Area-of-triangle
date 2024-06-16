178f7ead5e38ebfb2d5aaed28b9b2f40007680b2
RishuYadav220 <123231783+RishuYadav220@users.noreply.github.com>
2024-06-16 08:41:32 +0530

    Add files via upload

---
+++ First program.py
@@ -0,0 +1,10 @@
+#This is my first python program
+#To check the area of rectangle and triangle
+print("hello world")
+A = int(input("Enter the value of A: "))
+B = int(input("Enter the value of B: "))
+Area = A * B 
+print("Area of rectangle is ", Area)
+Area = 1/2*A*B 
+print("Area of triangle is ",Area)
+print("Good bye")
